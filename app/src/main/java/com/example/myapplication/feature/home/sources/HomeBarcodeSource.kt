59f760a1c0ad23392efa33792ba742f833ab8205
Marcelo Gobetti <mwgobetti@gmail.com>
2025-04-06 12:17:53 -0300

    Fix DI so it's ViewModelScoped

--- app/src/main/java/com/example/myapplication/feature/home/sources/HomeBarcodeSource.kt
+++ app/src/main/java/com/example/myapplication/feature/home/sources/HomeBarcodeSource.kt
@@ -4,7 +4,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
 import javax.inject.Inject
+
+@Module
+@InstallIn(ViewModelComponent::class)
+object HomeBarcodeSourceModule {
+    @Provides
+    @ViewModelScoped
+    fun provideBarcodeSource(): HomeBarcodeSource = HomeBarcodeSource()
+}
 
 class HomeBarcodeSource @Inject constructor() {
     private var barcode by mutableStateOf("0030000436073")
