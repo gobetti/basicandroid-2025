e40c46162af9b7f732cfbc52636722c4cae9c3e0
Marcelo Gobetti <mwgobetti@gmail.com>
2025-04-17 06:49:29 -0300

    Persist state across configuration changes

--- feature/home/src/main/kotlin/HomeAction.kt
+++ feature/home/src/main/kotlin/HomeAction.kt
@@ -1,9 +1,10 @@
 package com.example.feature.home
 
+import androidx.compose.ui.text.input.TextFieldValue
 import com.example.core.data.ProductData
 
 internal sealed interface HomeAction {
-    data class BarcodeChange(val barcode: String) : HomeAction
+    data class BarcodeChange(val textFieldValue: TextFieldValue) : HomeAction
     data object OpenList : HomeAction
     data class OpenProduct(val product: ProductData) : HomeAction
     data object Reset : HomeAction
